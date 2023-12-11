bbe780dbb7765b23a5fc574842aeb4911179b287
Kevin Esslinger <esslinger.k@northeastern.edu>
2023-12-11 18:23:29 +0100

    Update Day 1 test formatting

--- day1/day1_test.go
+++ day1/day1_test.go
@@ -2,22 +2,31 @@ package day1
 
 import (
 	"log"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDay1Part1(t *testing.T) {
-	sum, err := Part1("./data/day1_test.txt")
+	file, err := os.Open("./data/day1_test.txt")
 	if err != nil {
-		log.Fatalf("Error processing part 1: %s", err)
+		t.Fatalf("Cannot open file in Day 1 Part 1: %s", err)
+	}
+	sum, err := Part1(file)
+	if err != nil {
+		t.Fatalf("Error processing part 1: %s", err)
 	}
 	expected := 142
 	assert.Equal(t, expected, sum, "Sum is %d but should be %d", sum, expected)
 }
 
 func TestDay1Part2(t *testing.T) {
-	sum, err := Part2("./data/day1_part2_test.txt")
+	file, err := os.Open("./data/day1_part2_test.txt")
+	if err != nil {
+		t.Fatalf("Cannot open file in Day 1 Part 2: %s", err)
+	}
+	sum, err := Part2(file)
 	if err != nil {
 		log.Fatalf("Error processing part 2: %s", err)
 	}
